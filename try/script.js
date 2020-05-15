038ff9082f7121d73494c6767653480be01f117f
Alma Claudine Baluyot <a.claudine2398@gmail.com>
2020-05-15 15:57:46 +0800

    Implement New UI Changes for the Landing Page and View All (Latest Product)
    Implement  Landing Page and View All (Latest Product)

---
+++ try/script.js
@@ -0,0 +1,8 @@
+bulmaCarousel.attach('#trend-slide', {
+			slidesToScroll: 2,
+			slidesToShow: 4,
+      pagination: false,
+      loop: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+		});
